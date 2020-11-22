209e637edf43ba39ff54dcd70ede78353695d115
tangentstorm <michal.wallace@gmail.com>
2020-11-22 13:59:46 -0500

    assets from sandpile shoot

---
+++ s1/e2-sandpiles/assets/inheritance-diagram.vue
@@ -0,0 +1,159 @@
+<!-- Tufts VUE 3.3.0 concept-map (inheritance-diagram-copy.vue) 2020-01-06 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/C:/Program%20Files%20(x86)/VUE/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Mon Jan 06 14:40:50 EST 2020 by michal on platform Windows 8 6.2 in JVM 1.7.0_21-b11 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built October 8 2015 at 1658 by tomadm on Linux 2.6.32-504.23.4.el6.x86_64 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0"
+    label="inheritance-diagram-copy.vue" created="1578339281662" x="0.0"
+    y="0.0" width="350.56067" height="312.36493" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1578339650679"
+        spec="D:\ver\j-talks\s1\e2-sandpiles\inheritance-diagram-copy.vue"
+        type="1" xsi:type="URLResource">
+        <title>inheritance-diagram-copy.vue</title>
+        <property key="File" value="D:\ver\j-talks\s1\e2-sandpiles\inheritance-diagram-copy.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a54ac1fe1af008cb6a7204fa719</URIString>
+    <child ID="6" label="gridpad" layerID="1" created="1578339291645"
+        x="607.7611" y="201.0" width="98.0" height="44.0"
+        strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#D0D0D0</fillColor>
+        <strokeColor>#776D6D</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Calibri-plain-28</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a54ac1fe1af008cb6a77af56562</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="7" label="sandpiles" layerID="1" created="1578339354521"
+        x="598.0878" y="336.06067" width="117.0" height="44.0"
+        strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#83CEFF</fillColor>
+        <strokeColor>#776D6D</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Calibri-plain-28</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a55ac1fe1af008cb6a73ee1421c</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="8" label="sandcalc" layerID="1" created="1578339364377"
+        x="734.06067" y="468.36493" width="108.0" height="44.0"
+        strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#C1F780</fillColor>
+        <strokeColor>#776D6D</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Calibri-plain-28</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a55ac1fe1af008cb6a7744aac20</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="9" label="sandpaint" layerID="1" created="1578339364993"
+        x="492.5" y="465.60858" width="123.0" height="44.0"
+        strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#C1F780</fillColor>
+        <strokeColor>#776D6D</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Calibri-plain-28</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a55ac1fe1af008cb6a7f21ae84b</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="10" layerID="1" created="1578339388926" x="654.616"
+        y="243.0" width="4.1168213" height="95.0625" strokeWidth="4.0"
+        autoSized="false" controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a56ac1fe1af008cb6a70fd456a7</URIString>
+        <point1 x="656.616" y="336.0625"/>
+        <point2 x="656.73285" y="245.0"/>
+        <ID1 xsi:type="node">7</ID1>
+        <ID2 xsi:type="node">6</ID2>
+    </child>
+    <child ID="11" layerID="1" created="1578339390439" x="569.4216"
+        y="378.06073" width="71.74463" height="89.54785"
+        strokeWidth="4.0" autoSized="false" controlCount="0"
+        arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a57ac1fe1af008cb6a731558d5f</URIString>
+        <point1 x="571.4216" y="465.60858"/>
+        <point2 x="639.1662" y="380.06073"/>
+        <ID1 xsi:type="node">9</ID1>
+        <ID2 xsi:type="node">7</ID2>
+    </child>
+    <child ID="12" layerID="1" created="1578339392022" x="676.4496"
+        y="378.06067" width="91.74933" height="92.30426"
+        strokeWidth="4.0" autoSized="false" controlCount="0"
+        arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a58ac1fe1af008cb6a7aa0362fa</URIString>
+        <point1 x="766.1989" y="468.36493"/>
+        <point2 x="678.4496" y="380.06067"/>
+        <ID1 xsi:type="node">8</ID1>
+        <ID2 xsi:type="node">7</ID2>
+    </child>
+    <layer ID="1" label="Layer 1" created="1578339281665" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a59ac1fe1af008cb6a79cf5722e</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-14.0" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Untitled Pathway" created="1578339281661"
+            x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
+            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            <strokeColor>#B3993333</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a59ac1fe1af008cb6a73c2f6950</URIString>
+            <masterSlide ID="2" created="1578339281679" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a59ac1fe1af008cb6a796f0f1d2</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1578339282892" x="335.5" y="172.5"
+                    width="129.0" height="55.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a5aac1fe1af008cb6a7dad6c2b4</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1578339282893" x="346.5" y="281.5"
+                    width="107.0" height="37.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a5aac1fe1af008cb6a7abd96ba6</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1578339282895"
+                    x="373.5" y="384.0" width="53.0" height="32.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/7c5f0a5aac1fe1af008cb6a7c97a1f38</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2020-01-06</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>D:\ver\j-talks\s1\e2-sandpiles</saveLocation>
+    <saveFile>D:\ver\j-talks\s1\e2-sandpiles\inheritance-diagram-copy.vue</saveFile>
+</LW-MAP>
