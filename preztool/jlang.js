35e7c52081df3311a197fe3016b83c9c08a30e2d
tangentstorm <michal.wallace@gmail.com>
2020-12-16 16:09:46 -0500

    preztool: extract jlang.js + minor cleanups

---
+++ preztool/jlang.js
@@ -0,0 +1,123 @@
+/// Adverbs
+let A=[
+  '~',   // Reflex • Passive / Evoke
+  '/',   // Insert • Table
+  '/.',  // Oblique • Key
+  '\\',  // Prefix • Infix
+  '\\.', // Suffix • Outfix
+  '}',   // Item Amend • Amend
+  'b.',  // Boolean / Basic
+  'f.',  // Fix
+  'M.',  // Memo
+  't.',  // Taylor Coeff. (m t. u t.)
+  't:',  // Weighted Taylor
+]
+
+/// Conjunctions
+let C = [
+  '^:',  // Power (u^:n u^:v)
+  '.',   // Determinant • Dot Product
+  '..',  // Even
+  '.:',  // Odd
+  ':',   // Explicit / Monad-Dyad
+  ':.',  // Obverse
+  '::',  // Adverse
+  ';.',  // Cut
+  '!.',  // Fit (Customize)
+  '!:',  // Foreign
+  '"',   // Rank (m"n u"n m"v u"v)
+  '`',   // Tie (Gerund)
+  '`:',  // Evoke Gerund
+  '@',   // Atop
+  '@.',  // Agenda
+  '@:',  // At
+  '&',   // Bond / Compose
+  '&.',  // Under (Dual)
+  '&.:', // Under (Dual)
+  '&:',  // Appose
+  'd.',  // Derivative
+  'D.',  // Derivative
+  'D:',  // Secant Slope
+  'H.',  // Hypergeometric
+  'L:',  // Level At
+  'S:',  // Spread
+  'T.',  // Taylor Approximation
+];
+
+/// Verbs
+let V = [
+  '=',    // Self-Classify • Equal
+  '<',    // Box • Less Than
+  '<.',   // Floor • Lesser Of (Min)
+  '<:',   // Decrement • Less Or Equal
+  '>',    // Open • Larger Than
+  '>.',   // Ceiling • Larger of (Max)
+  '>:',   // Increment • Larger Or Equal
+  '_:',   // Infinity
+  '+',    // Conjugate • Plus
+  '+.',   // Real / Imaginary • GCD (Or)
+  '+:',   // Double • Not-Or
+  '*',    // Signum • Times
+  '*.',   // Length/Angle • LCM (And)
+  '*:',   // Square • Not-And
+  '-',    // Negate • Minus
+  '-.',   // Not • Less
+  '-:',   // Halve • Match
+  '%',    // Reciprocal • Divide
+  '%.',   // Matrix Inverse • Matrix Divide
+  '%:',   // Square Root • Root
+  '^',    // Exponential • Power
+  '^.',   // Natural Log • Logarithm
+  '$',    // Shape Of • Shape
+  '$.',   // Sparse
+  '$:',   // Self-Reference
+  '~.',   // Nub •
+  '~:',   // Nub Sieve • Not-Equal
+  '|',    // Magnitude • Residue
+  '|.',   // Reverse • Rotate (Shift)
+  '|:',   // Transpose
+  ',',    // Ravel • Append
+  ',.',   // Ravel Items • Stitch
+  ',:',   // Itemize • Laminate
+  ';',    // Raze • Link
+  ';:',   // Words • Sequential Machine
+  '#',    // Tally • Copy
+  '#.',   // Base 2 • Base
+  '#:',   // Antibase 2 • Antibase
+  '!',    // Factorial • Out Of
+  '/:',   // Grade Up • Sort
+  '\\:',   // Grade Down • Sort
+  '[',    // Same • Left
+  '[:',   // Cap
+  ']',    // Same • Right
+  '{',    // Catalogue • From
+  '{.',   // Head • Take
+  '{:',   // Tail •
+  '{::',  //  Map • Fetch
+  '}.',   // Behead • Drop
+  '}:',   // Curtail •
+  '".',   // Do • Numbers
+  '":',   // Default Format • Format
+  '?',    // Roll • Deal
+  '?.',   // Roll • Deal (fixed seed)
+  'A.',   // Anagram Index • Anagram
+  'C.',   // Cycle-Direct • Permute
+  'e.',   // Raze In • Member (In)
+  'E.',   // • Member of Interval
+  'i.',   // Integers • Index Of
+  'i:',   // Steps • Index Of Last
+  'I.',   // Indices • Interval Index
+  'j.',   // Imaginary • Complex
+  'L.',   // Level Of •
+  'o.',   // Pi Times • Circle Function
+  'p.',   // Roots • Polynomial
+  'p..',  // Poly. Deriv. • Poly. Integral
+  'p:',   // Primes
+  'q:',   // Prime Factors • Prime Exponents
+  'r.',   // Angle • Polar
+  's:',   // Symbol,
+  'u:',   // Unicode,
+  'x:',   // Extended Precision
+  '_9:',' _8:','_7:','_6:','_5:','_4:','_3:','_2:','_1:','0:',
+  '9:',  '8:', '7:', '6:', '5:', '4:', '3:', '2:', '1:', //  Constant Functions
+];
