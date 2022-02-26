59e47258dd9f30b85089d59a6f2e6e0a3bdd41d7
tangentstorm <michal.wallace@gmail.com>
2022-02-26 14:16:24 -0500

    got mandelbrot scenes working

--- wip/mandelbrot/main.gd
+++ wip/mandelbrot/main.gd
@@ -1,26 +1,22 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-func toggle(object):
+func toggle(node_name):
+	var object = find_node(node_name)
+	if object == null:
+		print("not found:", node_name)
+		return
 	object.visible = not object.visible
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	OS.window_per_pixel_transparency_enabled = true 
 	get_tree().get_root().set_transparent_background(true)
-	pass # Replace with function body.
 
 func _process(_delta):
 	if Input.is_action_just_pressed("toggle1"):
-		toggle($"viewmat")
+		toggle("viewmat")
 	if Input.is_action_just_pressed("toggle2"):
-		toggle($"complexplane")
+		toggle("complexplane")
 	if Input.is_action_just_pressed("toggle3"):
-		toggle($"mandelbrot")
+		toggle("mandelbrot")
 	if Input.is_action_just_pressed("toggle4"):
-		toggle($"gradient")
+		toggle("gradient")
