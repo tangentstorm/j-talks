698bea6ed89e03910244e2428553e73f07eebd76
tangentstorm <michal.wallace@gmail.com>
2022-03-01 19:02:08 -0500

    SceneTitle: encapsulate in .reveal(title)

--- assets/SceneTitle.gd
+++ assets/SceneTitle.gd
@@ -1,4 +1,7 @@
 tool extends Node2D
+# Animated Scene Title
+
+signal animation_finished
 
 export (String) var text setget set_text
 export (int) var text_len setget set_text_len
@@ -6,30 +9,43 @@ export var stripe_x = 0 setget set_stripe_x
 export var stripe_y = 0 setget set_stripe_y
 export var stripe_w = 0 setget set_stripe_w
 export var stripe_h = 0 setget set_stripe_h
+export (Color) var stripe_color = Color.silver setget set_stripe_color
+
+onready var anim = $"animation"
+onready var stripe = $stripe
+onready var label = $label
+
+func _ready():
+	anim.play('init')
+	yield(anim, "animation_finished")
+
+func reveal(text):
+	# does title animation and then emits 'animation_finished'
+	self.text = text
+	self.text_len = 0
+	anim.play("reveal"); yield(anim, "animation_finished")
+	anim.play('init'); yield(anim, "animation_finished")
+	emit_signal('animation_finished')
 
 func set_stripe_y(y):
 	stripe_y = y
-	if not is_inside_tree(): return # TODO: clean this up
-	if $stripe != null:
-		$stripe.rect_position.y = y
+	if stripe: stripe.rect_position.y = y
 	
 func set_stripe_x(x):
 	stripe_x = x
-	if not is_inside_tree(): return # TODO: clean this up
-	if $stripe != null:
-		$stripe.rect_position.x = x
+	if stripe: stripe.rect_position.x = x
 
 func set_stripe_w(w):
 	stripe_w = w
-	if not is_inside_tree(): return # TODO: clean this up
-	if $stripe != null:
-		$stripe.rect_size.x = w
+	if stripe: stripe.rect_size.x = w
 	
 func set_stripe_h(h):
 	stripe_h = h
-	if not is_inside_tree(): return # TODO: clean this up
-	if $stripe != null:
-		$stripe.rect_size.y = h
+	if stripe: stripe.rect_size.y = h
+
+func set_stripe_color(c):
+	stripe_color = c
+	if stripe: stripe.color = c
 
 func set_text(t):
 	text = t
@@ -37,12 +53,6 @@ func set_text(t):
 		
 func set_text_len(n):
 	text_len = int(n)
-	if text == null or text == '': return
-	if not is_inside_tree(): return # TODO: clean this up
-	if $label == null: return
+	if not text or not label: return
 	if n > 0: $label.text = text.substr(0,n)
 	else: $label.text = ""
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
