d6137034cf83f5521ee253541bf23ff9f299fbc8
tangentstorm <michal.wallace@gmail.com>
2022-07-28 21:53:38 -0400

    start on plot animations

--- wip/collatz/Axes.gd
+++ wip/collatz/Axes.gd
@@ -1,7 +1,27 @@
 tool extends Control
 
-const gh = 30
-const gw = 30
+export var line_color:Color = Color.cyan setget set_line_color
+export var axis_color:Color = Color.blue setget set_axis_color
+export var unit: Vector2 = Vector2(30,30) setget set_unit
+
+
+
+func set_line_color(c:Color):
+	line_color = c
+	refresh()
+
+func set_axis_color(c:Color):
+	axis_color = c
+	refresh()
+	
+func set_unit(u:Vector2):
+	unit = u
+	refresh()
+	
+func refresh():
+	update()
+	for child in get_children():
+		child.update()
 
 var gymax
 var gxmax
@@ -11,12 +31,12 @@ var xmin
 var xmax
 
 func _ready():
-	gymax = floor(rect_size.y / gh)
-	gxmax = floor(rect_size.x / gw)
+	gymax = floor(rect_size.y / unit.y)
+	gxmax = floor(rect_size.x / unit.x)
 	ymax = floor(rect_size.y)
-	ymin = ymax - (gymax * gh)
+	ymin = ymax - (gymax * unit.y)
 	xmin = 0
-	xmax = gxmax * gw
+	xmax = gxmax * unit.x
 
 func origin() -> Vector2:
 	_ready()
@@ -24,9 +44,8 @@ func origin() -> Vector2:
 
 func _draw():
 	_ready()
-	print("xmin:", xmin, " max: ", xmax, " ymin:", ymin, " max: ", ymax) 
 	for x in range(gxmax+1):
-		draw_line(Vector2(xmin+x*gw, ymin), Vector2(xmin+x*gw, ymax), Color.blueviolet)
+		draw_line(Vector2(xmin+x*unit.x, ymin), Vector2(xmin+x*unit.x, ymax), line_color)
 	for y in range(gymax+1):
-		draw_line(Vector2(xmin, ymin+y*gh), Vector2(xmax, ymin+y*gh), Color.blueviolet)
+		draw_line(Vector2(xmin, ymin+y*unit.y), Vector2(xmax, ymin+y*unit.y), line_color)
 		
--- wip/collatz/LinePlot.gd
+++ wip/collatz/LinePlot.gd
@@ -1,28 +1,42 @@
 tool extends Control
 
-const gw = 30
-const gh = 30
+export var b:float = 0.0 setget set_b
+export var m:float = 1.0 setget set_m
+export var dist:float = 0 setget set_dist
+export var nk: Vector2 = Vector2(1, 0) setget set_nk   # when to draw dots (ex: 2 1 = odd)
+export var color:Color = Color.cornflower setget set_color
 
-export var b = 1 setget set_b
-export var m = 3 setget set_m
+func set_color(c):
+	color = c
+	update()
 
 func set_b(new_b):
 	b = new_b
-	_draw()
+	update()
 	
 func set_m(new_m):
 	m = new_m
-	_draw()
+	update()
+
+func set_dist(d):
+	dist = d
+	update()
+	
+func set_nk(v):
+	nk = v
+	update()
 
 func _draw():
 	var origin : Vector2 = get_parent().origin()
-	var px; var py
+	var unit = get_parent().unit
+	var px = 0; var py = -unit.y*b
 	for x in range(50):
 		var y = b + m * x
-		var gx = gw * x
-		var gy = -gh * y
-		draw_circle(origin + Vector2(gx, gy), 5, Color.cornflower)
-		if x > 0:
-			draw_line(origin+Vector2(px, py),
-					  origin+Vector2(gx, gy), Color.cornflower, 2.5)
-		px = gx; py = gy
+		var gx = unit.x * x
+		var gy = -unit.y * y
+		if x % int(nk.x) == int(nk.y):
+			draw_circle(origin + Vector2(gx, gy), 5, color)
+			if (x > 0) and (x < dist):
+				draw_line(origin+Vector2(px, py),
+						  origin+Vector2(gx, gy), color, 2.5)
+			px = gx; py = gy
