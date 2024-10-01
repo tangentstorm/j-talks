78573c01d9432b4c1a3f2a71f1077d9a217cf244
tangentstorm <michal.wallace@gmail.com>
2024-10-01 10:59:48 -0400

    move avatar stuff to separate repo

--- assets/michal/Michal-Lipsync.gd
+++
@@ -1,9 +0,0 @@
-extends AnimationPlayer
-
-	
-func _input(event):
-	if Input.is_action_just_pressed("ui_accept"):
-		play("lipsync.000")
-		queue('blink')
-		queue("eyebrow-move_to_front")
-		queue("lipsync.001")
